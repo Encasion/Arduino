d82259c32b6e5a608facc22e733b90104ffb07bb
Encasion <60199691+Encasion@users.noreply.github.com>
2020-01-22 23:21:19 +0200

    Add files via upload

---
+++ project/project.ino
@@ -0,0 +1,29 @@
+void setup(){
+  pinMode(10, OUTPUT);
+  digitalWrite(10,0 );
+    pinMode(9, OUTPUT);
+  digitalWrite(9,0);
+    pinMode(8, OUTPUT);
+  digitalWrite(8,0);
+    pinMode(7, OUTPUT);
+  digitalWrite(7,0);
+    pinMode(6, OUTPUT);
+  digitalWrite(6,0);
+    pinMode(5, OUTPUT);
+  digitalWrite(5,0);
+    pinMode(4, OUTPUT);
+  digitalWrite(4,0);
+    pinMode(3, OUTPUT);
+  digitalWrite(3,0);
+    pinMode(2, OUTPUT);
+  digitalWrite(2,0);
+    pinMode(11, OUTPUT);
+  digitalWrite(11,0);
+    pinMode(12, OUTPUT);
+  digitalWrite(12,0);
+    pinMode(13, OUTPUT);
+  digitalWrite(13,0);
+  }
+  void loop(){
+    
+    }
